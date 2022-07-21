1f680093663bb63687290753124518c369a9ac2e
VivekPanchal <ivivekpanchal123@gmail.com>
2022-07-21 13:15:54 +0530

    nav bar and login module done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbComponent } from './navb/navb.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
