8414301b1b0fc78b6a2f3e53b162829f82fa3096
VivekPanchal <ivivekpanchal123@gmail.com>
2022-07-22 13:50:31 +0530

    form middle and border radius

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from "@angular/router";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbComponent } from './navb/navb.component';
 import { LoginComponent } from './login/login.component';
+import { TopNavComponent } from './Layout/top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbComponent,
-    LoginComponent
+    LoginComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'login', component:LoginComponent },
+      { path: 'layout', component:TopNavComponent },
+      { path: '**', redirectTo: '/layout'}
+  ])],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/navb/navb.component.ts
+++ src/app/navb/navb.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  } from "@material/textfield";
 
 @Component({
   selector: 'app-navb',
